c4d653341e5ced43546238ef54a39d82597d56ac
cunivca <ivana@edoo.is>
2022-11-12 16:18:21 +0100

    manifest

--- birthday/__manifest__.py
+++ birthday/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Birthday',
     'complexity': 'easy',
     'Summary': 'A Module For a birthday wishes',
-    'depends': ['base','svatky'],
+    'depends': ['base','svatky','mail'],
     'data': [
         'views/partner_views.xml',
         'data/mail_template_data.xml',
