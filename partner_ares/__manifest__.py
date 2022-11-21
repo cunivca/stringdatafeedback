7f602b051040de93ea6b6b0d6e42eec0d3c93d99
cunivca <ivana@edoo.is>
2022-11-21 21:45:36 +0100

    test

--- partner_ares/__manifest__.py
+++ partner_ares/__manifest__.py
@@ -20,12 +20,6 @@
             'partner_ares/static/src/scss/*',
             'partner_ares/static/src/js/*',
             'partner_ares/static/src/xml/*',
-            # tento import v originalnim v modulu partner_autocomplete neni, nicmene taky nepomohl
-            'partner_ares/static/src/lib/jsvat.js',
-        ],
-        # Pridano dle https://stackoverflow.com/questions/73103280/odoo15-how-can-i-load-js-in-qweb-template
-        'web.assets_frontend': [
-            'partner_ares/static/src/lib/jsvat.js',
         ],
         'web.tests_assets': [
             'partner_ares/static/lib/**/*',
